9a9f863cf32b2179136a744835bda1dff855f1c4
Cristhian Medina <cristhian.medina.or@hotmail.com>
2025-03-18 09:01:29 -0500

    install express

--- Proyect_CRUD/src/index.js
+++ Proyect_CRUD/src/index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo');
+import express from 'express';
+
+//Initialize
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Run server
+app.listen(app.get('port'), () => {
+  console.log('Server lisening on port', app.get('port'));
+});
+
